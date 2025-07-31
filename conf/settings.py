6fbcc8f5918eaacda156142c2e2ca9e0671f8ecf
shaxzo17 <shaxzodaavalboyeva14@gmail.com>
2025-07-31 06:13:02 +0300

    added .env and .gitignore for deployment

--- conf/settings.py
+++ conf/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 from django.conf.global_settings import STATICFILES_DIRS
 
@@ -21,13 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w48$5sq1b0v855f)^k+6)(ftqm35@$q8up-mcd4#^$sjc0f1t('
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
